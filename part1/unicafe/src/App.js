0ebe7ffcbab915555be1213c71f4623566730b94
sergioruizsan <srs.bcn@gmail.com>
2022-06-12 08:36:15 +0100

    part1 - 1.10: unicafe step5

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,6 +1,14 @@
 import { useState } from "react"
 
 
+const Button = ({clickHandler, text}) => (
+  <button onClick={clickHandler}>{text}</button>
+)
+
+const StatisticLine = ({text, value}) => (
+  <div>{text} {value}</div>
+)
+
 const Statistics = ({good, bad, neutral}) => {
 
   const total = good + neutral + bad
@@ -11,12 +19,12 @@ const Statistics = ({good, bad, neutral}) => {
 
   return (
     <>
-      <div>good {good}</div>
-      <div>neutral {neutral}</div>
-      <div>bad {bad}</div>
-      <div>all {total}</div>
-      <div>average {Number.isNaN(average) ? 0 : average}</div>
-      <div>positive {Number.isNaN(positive) ? 0 : positive} %</div>
+      <StatisticLine text="good" value={good}/>
+      <StatisticLine text="neutral" value={neutral}/>
+      <StatisticLine text="bad" value={bad}/>
+      <StatisticLine text="all" value={total}/>
+      <StatisticLine text="average" value={average}/>
+      <StatisticLine text="positive" value={`${positive} %`}/>
     </>
   )
 
@@ -34,9 +42,9 @@ const App = () => {
     <div>
       <h1>Unicafe</h1>
       <h2>give feedback</h2>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <Button clickHandler={() => setGood(good + 1)} text="good"/>
+      <Button clickHandler={() => setNeutral(neutral + 1)} text="neutral"/>
+      <Button clickHandler={() => setBad(bad + 1)} text="bad"/>
       <h2>statistics</h2>
       <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
