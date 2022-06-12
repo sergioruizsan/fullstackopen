c6173a4d2ab32ca6f5fb77d52ad82a39d11fe2a1
sergioruizsan <srs.bcn@gmail.com>
2022-06-12 08:09:14 +0100

    1.7: unicafe step2

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,6 +6,10 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const total = good + neutral + bad
+  const average = (good - bad) / total
+  const positive = good / total * 100
+
   return (
     <>
     <div>
@@ -18,6 +22,9 @@ const App = () => {
       <div>good {good}</div>
       <div>neutral {neutral}</div>
       <div>bad {bad}</div>
+      <div>all {total}</div>
+      <div>average {Number.isNaN(average) ? 0 : average}</div>
+      <div>positive {Number.isNaN(positive) ? 0 : positive} %</div>
     </div>
     </>
   )
